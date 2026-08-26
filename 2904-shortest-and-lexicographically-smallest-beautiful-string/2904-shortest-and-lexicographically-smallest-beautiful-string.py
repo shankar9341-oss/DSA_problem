4d2e185eb8dad80d2e8eda210c0ea213e8b0675e
Shankar Das <shankardas9341@gmail.com>
2026-08-26 19:42:45 +0530

    Time: 0 ms (100%), Space: 19.3 MB (30.63%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,36 +1,21 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
-        # result = " "
-        # left = 0
-        # count = 0
-        # for right in range(len(s)):
-        #     if s[right] == "1": 
-        #         count += 1
+        result = ''
+        left = 0
+        count = 0
+        for right in range(len(s)):
+            if s[right] == "1": 
+                count += 1
             
-        #     # while left < right and (x > k or s[right] == "0"):
-        #     while count > k:
-        #         if s[left] == "1":
-        #             left -= 1
-        #         left += 1
+            while left < right and (count > k or s[left] == "0"):
+                if s[left] == "1": 
+                    count -= 1
+                left += 1
 
-        #     if count == k:
-        #         s1 = s[left:right + 1]
-        #         if not result or len(s1) < len(result) or (len(s1) == len(result) and s1 < result):
-        #             result = s1
-        # return result
+            if count == k:
+                s1 = s[left:right + 1]
+                if not result or len(s1) < len(result) or (len(s1) == len(result) and s1 < result):
+                    result = s1
+        return result
 
-        n = len(s)
-        res = ''
-        x = 0
-        l = 0
-        for r in range(n):
-            if s[r] == '1': x += 1
-            while l < r and (x > k or s[l] == '0'):
-                if s[l] == '1': x -= 1
-                l += 1
-            if x == k:
-                ss = s[l : r+1]
-                if not res or len(ss) < len(res) or (len(ss) == len(res) and ss < res):
-                    res = ss
-
-        return res
+     
