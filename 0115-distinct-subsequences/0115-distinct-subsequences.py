1f8486cb7b7e9a81d51a4c962257c32505ff1daa
Shankar Das <shankardas9341@gmail.com>
2026-09-06 09:41:07 +0530

    Time: 927 ms (5.28%), Space: 232.3 MB (18.92%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -2,12 +2,9 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         mapp = {}
         def dfs(i, j):
-            if j == len(t):
-                return 1
-            if i == len(s):
-                return 0
-            if (i,j) in mapp:
-                return mapp[(i,j)]
+            if j == len(t): return 1
+            if i == len(s): return 0
+            if (i,j) in mapp: return mapp[(i,j)]
             
             if s[i] == t[j]:
                 mapp[(i,j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
@@ -15,5 +12,5 @@ class Solution:
                 mapp[(i,j)] = dfs(i + 1, j)
 
             return mapp[(i,j)]
-            
+
         return dfs(0, 0)
