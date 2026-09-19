e08106c76c70b3808e03e9c0b509848e7853d66a
Shankar Das <shankardas9341@gmail.com>
2026-09-19 16:58:09 +0530

    Time: 379 ms (64.73%), Space: 19.4 MB (16.08%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -28,28 +28,3 @@ class Solution:
         
         
         
-        
-        
-        
-        # nums.sort()
-        # closest = float("inf")
-
-        # for i in range(len(nums)):
-        #     if i > 0 and nums[i] == nums[i-1]:
-        #         continue
-        #     left = i+1
-        #     right = len(nums)-1
-
-        #     while left < right:
-        #         curr_sum = nums[i] + nums[left] + nums[right]
-        #         if abs(curr_sum - target) < abs(closest - target):
-        #             closest = curr_sum  
-        #         if curr_sum > target:
-        #             right -= 1
-        #         elif curr_sum < target:
-        #             left += 1
-        #         else:
-        #             return curr_sum
-
-        # return closest
-        
