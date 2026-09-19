66517b91d8f2050f2ed2bf8ef7b5c14b2a9466a6
Shankar Das <shankardas9341@gmail.com>
2026-09-19 16:57:58 +0530

    Time: 367 ms (77.24%), Space: 19.3 MB (47.09%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,24 +1,55 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        closest = float("inf")
-
+        ans = float("inf")
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            left = i+1
+            left = i + 1
             right = len(nums)-1
-
             while left < right:
-                curr_sum = nums[i] + nums[left] + nums[right]
-                if abs(curr_sum - target) < abs(closest - target):
-                    closest = curr_sum  
-                if curr_sum > target:
-                    right -= 1
-                elif curr_sum < target:
+                total = nums[i] + nums[left] + nums[right]
+                if abs(ans - target) > abs(total - target):
+                    ans = total
+                if total < target:
                     left += 1
+                elif total > target:
+                    right -= 1
                 else:
-                    return curr_sum
+                    return total
+        return ans
 
-        return closest
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # nums.sort()
+        # closest = float("inf")
+
+        # for i in range(len(nums)):
+        #     if i > 0 and nums[i] == nums[i-1]:
+        #         continue
+        #     left = i+1
+        #     right = len(nums)-1
+
+        #     while left < right:
+        #         curr_sum = nums[i] + nums[left] + nums[right]
+        #         if abs(curr_sum - target) < abs(closest - target):
+        #             closest = curr_sum  
+        #         if curr_sum > target:
+        #             right -= 1
+        #         elif curr_sum < target:
+        #             left += 1
+        #         else:
+        #             return curr_sum
+
+        # return closest
         
